0b8da16d66c800baa3dc7e4e028d85ea1b5a6ee1
Tobikous <tobinanonano1996@gmail.com>
2023-03-03 19:02:41 +0900

    Japanese error message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -102,7 +102,7 @@
 
 						<a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 							href="{{ route('login') }}">
-							{{ __('Already registered?') }}
+							{{ __('既に会員登録していますか?') }}
 						</a>
 
 						<button type="submit"
