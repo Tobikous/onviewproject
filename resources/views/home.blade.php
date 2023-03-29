0b2f6cf4873dcd6cb67085a6ad65b1899937de1d
Tobikous <tobinanonano1996@gmail.com>
2023-03-29 11:56:42 +0900

    Layout fine-tuning

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,41 +33,45 @@
 
 		<section class="text-gray-600 body-font mb-10">
 			<div class="container px-5 py-10 mx-auto">
-				<h1 class="title-font text-3xl  font-medium text-gray-900 mb-5">投稿レビュー一覧</h1>
-				<div class="flex flex-wrap -m-4">
+				<h1 class="title-font text-3xl  font-medium text-gray-900 mb-5 text-center">投稿レビュー一覧</h1>
+				<div class="flex flex-wrap m-5">
 					@foreach($reviews AS $review)
+
 					<div class="p-4 lg:w-1/3">
 						<div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-8 pb-10 rounded-lg text-center">
-							<img alt="gallery" class="lg:h-48 md:h-36 w-full object-cover object-center"
-								src="{{$review['image']}}">
-							<h2 class="text-yellow-500 tracking-widest text-s title-font font-medium mt-3">
-								{{$review['star']}}
-							</h2>
-							<h1
-								class="title-font sm:text-xl text-xl font-medium text-gray-600 mb-2.5 line-clamp-2 overflow-hidden">
-								{{$review['onsenName']}}</h1>
-							<p class="leading-relaxed mb-2 line-clamp-3">{{$review['content']}}</p>
-							<a class="text-yellow-500 font-medium hover:text-yellow-600 inline-flex items-center pt-1.5"
-								href="/show/{{$review['id']}}">レビュー詳細
-								<svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
-									fill="none" stroke-linecap="round" stroke-linejoin="round">
-									<path d="M5 12h14"></path>
-									<path d="M12 5l7 7-7 7"></path>
-								</svg>
+							<a href="/show/{{$review['id']}}">
+								<img alt="blog" class="lg:h-48 md:h-36 w-full object-cover object-center "
+									src="{{$review['image']}}">
+								<h2 class="text-yellow-500 tracking-widest text-s title-font font-medium mt-3">
+									{{$review['star']}}
+								</h2>
+								<h1
+									class="title-font sm:text-xl text-xl font-medium text-gray-600 mb-2.5 line-clamp-2 overflow-hidden">
+									{{$review['onsenName']}}</h1>
+								<p class="leading-relaxed mb-2 line-clamp-3">{{$review['content']}}</p>
+								<a class="text-yellow-500 font-medium hover:text-yellow-600 inline-flex items-center pt-1.5"
+									href="/show/{{$review['id']}}">レビュー詳細
+									<svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
+										fill="none" stroke-linecap="round" stroke-linejoin="round">
+										<path d="M5 12h14"></path>
+										<path d="M12 5l7 7-7 7"></path>
+									</svg>
+
+								</a>
+								<span
+									class="text-gray-400 items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm border-gray-200">
+									<svg class="w-4 h-4"></svg>更新日：{{$review['updated_at']->format('Y年m月d日')}}
+								</span>
 
 							</a>
-							<span
-								class="text-gray-400 items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm border-gray-200">
-								<svg class="w-4 h-4"></svg>更新日：{{$review['updated_at']->format('Y年m月d日')}}
-							</span>
-
-
 						</div>
 					</div>
 					@endforeach
 				</div>
-				<a class="float-right mt-5 text-blue-500 inline-flex items-center font-medium"
-					href="article">▶全てのレビューを見る</a>
+				<div class="flex justify-center">
+					<a class="mt-5 bg-blue-500 text-white py-4 px-5 rounded inline-flex items-center font-medium text-xl"
+						href="article">全てのレビューを見る</a>
+				</div>
 			</div>
 		</section>
 
@@ -76,35 +80,37 @@
 		@auth
 		<section class="text-gray-600 body-font">
 			<div class="container px-5 py-24 mx-auto">
-				<h1 class="title-font text-3xl font-medium text-gray-900 mb-5">自分が投稿したレビュー</h1>
-				<div class="flex flex-wrap -m-4">
+				<h1 class="title-font text-3xl font-medium text-gray-900 mb-5 text-center">自分が投稿したレビュー</h1>
+				<div class="flex flex-wrap m-5">
 					@foreach($reviews AS $review)
 					@if($review->isWrittenByUser(auth()->user()))
 					<div class="p-4 md:w-1/3">
 						<div class="h-full border-2 border-gray-300 border-opacity-60 rounded-lg overflow-hidden">
-							<img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{{$review['image']}}"
-								alt="blog">
-							<div class="p-6">
-								<h1 class="title-font text-lg font-medium text-gray-900 ">{{$review['onsenName']}}
-								</h1>
-								<h2 class="text-yellow-500 tracking-widest text-s title-font font-medium  mb-3">
-									{{$review['star']}}
-								</h2>
-								<p class="leading-relaxed mb-3 line-clamp-3">{{$review['content']}}</p>
-								<div class="flex items-center flex-wrap ">
-									<a class="text-yellow-500 font-medium inline-flex items-center md:mb-2 lg:mb-0 p-1"
-										href="/show/{{$review['id']}}">レビュー詳細
-										<svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor"
-											stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-											<path d="M5 12h14"></path>
-											<path d="M12 5l7 7-7 7"></path>
-										</svg>
-									</a>
-									<span
-										class="text-gray-400 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm py-1 border-gray-200">
-										<svg class="w-4 h-4"></svg>更新日：{{$review['updated_at']->format('Y年m月d日')}}
-									</span>
-									<!-- <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+							<a href="/show/{{$review['id']}}">
+								<img class="lg:h-48 md:h-36 w-full object-cover object-center"
+									src="{{$review['image']}}" alt="blog">
+								<div class="p-6">
+									<h1 class="title-font text-lg font-medium text-gray-900 ">{{$review['onsenName']}}
+									</h1>
+									<h2 class="text-yellow-500 tracking-widest text-s title-font font-medium  mb-3">
+										{{$review['star']}}
+									</h2>
+									<p class="leading-relaxed mb-3 line-clamp-3">{{$review['content']}}</p>
+									<div class="flex items-center flex-wrap ">
+										<a class="text-yellow-500 font-medium inline-flex items-center md:mb-2 lg:mb-0 p-1"
+											href="/show/{{$review['id']}}">レビュー詳細
+											<svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor"
+												stroke-width="2" fill="none" stroke-linecap="round"
+												stroke-linejoin="round">
+												<path d="M5 12h14"></path>
+												<path d="M12 5l7 7-7 7"></path>
+											</svg>
+										</a>
+										<span
+											class="text-gray-400 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm py-1 border-gray-200">
+											<svg class="w-4 h-4"></svg>更新日：{{$review['updated_at']->format('Y年m月d日')}}
+										</span>
+										<!-- <span class="text-gray-400 inline-flex items-center leading-none text-sm">
 										<svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
 											stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
 											<path
@@ -112,8 +118,9 @@
 											</path>
 										</svg>6
 									</span> -->
+									</div>
 								</div>
-							</div>
+							</a>
 						</div>
 					</div>
 					@endif
