ce229a688fb6ca89a6b9b2aa0f965efaabd43808
Tobikous <tobinanonano1996@gmail.com>
2023-04-09 14:29:46 +0900

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
 			@guest
 			<button
 				class=" m-auto text-white bg-orange-500 border-0 py-8 px-20 focus:outline-none hover:bg-orange-600 rounded font-semibold text-4xl"
-				onclick="location.href='{{ route('login') }}'">ログインはこちらから</button>
+				onclick="location.href='{{ route('login') }}'">ログインはこちらから！</button>
 			@endguest
 
 		</div>
