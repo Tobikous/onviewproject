815da0c34229991d0c311cc3226a5696d0e9d69d
Tobikous <tobinanonano1996@gmail.com>
2023-04-12 00:30:36 +0900

    Enable HTTPS suppor

---
+++ app/Http/Middleware/HttpsProtocolMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HttpsProtocolMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->secure() && env('APP_ENV') === 'production') {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
