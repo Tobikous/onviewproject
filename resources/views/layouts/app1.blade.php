d3df622b80ec546c7f74aaf390599b9934a3abb4
Tobikous <tobinanonano1996@gmail.com>
2023-04-21 22:17:46 +0900

    fixed blade

--- resources/views/layouts/app1.blade.php
+++ resources/views/layouts/app1.blade.php
@@ -221,9 +221,10 @@
 
 	<footer class="bg-gray-100">
 		<div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-			<a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"><svg version="1.1" id="_x32_"
-					xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"
-					class="w-12 h-12 opacity-1" xml:space="preserve">
+			<a href="{{ url('/') }}" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"><svg
+					version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg"
+					xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" class="w-12 h-12 opacity-1"
+					xml:space="preserve">
 					<style type="text/css">
 						.st0 {
 							fill: #4B4B4B;
@@ -275,28 +276,8 @@
 				<a href=# rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@Tobikous</a>
 			</p>
 			<span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-				<a class="text-gray-500">
-					<svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-						class="w-5 h-5" viewBox="0 0 24 24">
-						<path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-					</svg>
-				</a>
-				<a class="ml-3 text-gray-500">
-					<svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-						class="w-5 h-5" viewBox="0 0 24 24">
-						<path
-							d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
-						</path>
-					</svg>
-				</a>
-				<a class="ml-3 text-gray-500">
-					<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-						stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-						<rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-						<path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-					</svg>
-				</a>
-				<a class="ml-3 text-gray-500">
+
+				<a href="https://www.linkedin.com/in/tobiyama-a40369271" class="ml-3 text-gray-500">
 					<svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
 						stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
 						<path stroke="none"
