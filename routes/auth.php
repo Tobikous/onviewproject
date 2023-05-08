32029ac53e68b4da48d874fe413dafd0d18089b0
Tobikous <tobinanonano1996@gmail.com>
2023-05-09 03:59:04 +0900

    ログイン回数の上限を設定

--- routes/auth.php
+++ routes/auth.php
@@ -24,7 +24,9 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])
+                ->middleware('auth.login:5,10')
+                ->name('login.store');
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
